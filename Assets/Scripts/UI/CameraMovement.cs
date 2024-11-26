ddc3d4eab96ea9ee28c148ebf13fd7fe20441b7b
Ayroh <b.yalcinkaya2019@gtu.edu.tr>
2024-11-26 16:07:51 +0300

    Camera movement updated to move according to camera position not mouse position.

--- Assets/Scripts/UI/CameraMovement.cs
+++ Assets/Scripts/UI/CameraMovement.cs
@@ -7,14 +7,15 @@ using Utilities.Signals;
 public class CameraMovement : MonoBehaviour
 {
     
-    private Vector3 startMousePos, startCameraPos, lastMousePos;
+    private Vector3 startMousePos, startCameraPos, lastCameraPos;
     private Vector2 movementConstantRatioWithCamera;
     private IEnumerator driftCoroutine;
 
-    private const float speedConstant = 0.05f;
+    private const float speedConstant = .5f;
     private const float maximumSpeed = 4f;
-    private const float minimumDriftStartSpeed = .2f;
-
+    private const float minimumDriftStartSpeed = .35f;
+    private const float driftEndSpeed = .01f;
+    
     private bool clicked = false;
 
 
@@ -45,14 +46,13 @@ public class CameraMovement : MonoBehaviour
         }
         else if(clicked && Input.GetMouseButton(0)) 
         {
+            lastCameraPos = transform.position;
+
             Vector3 currentFrameInput = Input.mousePosition;
             float xDiff = startMousePos.x - currentFrameInput.x;
             float yDiff = startMousePos.y - currentFrameInput.y;
 
-            //transform.position = startCameraPos + new Vector3(xDiff * movementConstantRatioWithCamera.x, 0, yDiff * movementConstantRatioWithCamera.y);
             transform.position = Vector3.Lerp(transform.position, startCameraPos + new Vector3(xDiff * movementConstantRatioWithCamera.x, 0, yDiff * movementConstantRatioWithCamera.y), .25f);
-
-            lastMousePos = Input.mousePosition;
         }
         else if (clicked && Input.GetMouseButtonUp(0))
         {
@@ -65,18 +65,17 @@ public class CameraMovement : MonoBehaviour
 
     private IEnumerator Drift()
     {
-        Vector3 lastFrameVector = (lastMousePos - Input.mousePosition);
+        Vector3 lastFrameVector = (transform.position - lastCameraPos);
         float speed = lastFrameVector.magnitude * speedConstant;
-        speed = Mathf.Clamp(speed, 0f, maximumSpeed);
 
+        speed = Mathf.Clamp(speed, 0f, maximumSpeed);
         if (speed < minimumDriftStartSpeed)
             yield break;
 
         Vector3 direction = lastFrameVector.normalized;
-        direction.z = direction.y;
         direction.y = 0;
 
-        while(speed > .1f)
+        while(speed > driftEndSpeed) 
         {
             speed *= .9f;
             transform.position += speed * direction;
